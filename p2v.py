67d0aeead6980cdc8dd22e7b052ed405ccf22f85
KOVAC Arnaud <arnaud.kovac@gmail.com>
2012-05-09 18:57:35 +0200

    Modification des options de ssh

--- p2v.py
+++ p2v.py
@@ -10,13 +10,14 @@ class Ssh:
     self.server = server
 
   def del_keyfile(self):
-     os.popen("ssh-keygen -R %s" % self.server,"r")
+     os.popen("ssh-keygen -R %s 2>&1 > dev/null" % self.server,"r")
 
   def copy_id(self):
+     print "### Copie des clefs : ###\n"
      os.popen("ssh-copy-id %s" % self.server,"r")
 
   def exec_cmd(self,cmd=''):
-    CMD = os.popen("ssh -o \"StrictHostKeyChecking=no\" root@%s '%s'" % (self.server,cmd),"r")
+    CMD = os.popen("ssh -o 'StrictHostKeyChecking=no' root@%s '%s'" % (self.server,cmd),"r")
     ret = CMD.readlines()
     return ret
 
@@ -317,6 +318,7 @@ class xen_host:
   def prep_cmd_ssh_key(self):
     AFFICHE_SSH = "\n"
     AFFICHE_SSH += "echo \"Password pour transfert de clefs ssh\"\n"
+    AFFICHE_SSH += "ssh-keygen -R "+ self.new_name_vm_ip +" 2>&1 > dev/null\n"
     AFFICHE_SSH += "ssh-copy-id "+ self.new_name_vm_ip +""
     return AFFICHE_SSH
 
