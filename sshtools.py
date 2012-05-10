860169a1ab7ed9fd1ba8686db7ab5a92caea7cad
Arnaud Kovac <akovac@akovac.vbur.b1.p.fti.net>
2012-05-10 15:28:35 +0200

    Modif de exec_cmd

--- sshtools.py
+++ sshtools.py
@@ -15,7 +15,6 @@ class Ssh:
      os.popen("ssh-copy-id %s" % self.server,"r")
 
   def exec_cmd(self,cmd=''):
-    self.del_keyfile()
     CMD = os.popen("ssh -o 'StrictHostKeyChecking=no' root@%s '%s'" % (self.server,cmd),"r")
     ret = CMD.readlines()
     return ret
