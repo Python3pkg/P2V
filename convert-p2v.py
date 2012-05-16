de8190cf417d017ea97faed802fbdcd081364ddb
KOVAC Arnaud <arnaud.kovac@gmail.com>
2012-05-16 02:04:14 +0200

    suppression d'un caractere inutile

--- convert-p2v.py
+++ convert-p2v.py
@@ -30,7 +30,7 @@ def P2V_HVM_PHASE_2(VM):
 
 
 def analyse_commande():
-  parser = OptionParser(usage="%convert-p2v.py -f <FQDN>  [-i <IP>] | [-v <Num_VLAN>] | [-c <Num_Demande_Sysadmin>]_", version="%prog 1.0")
+  parser = OptionParser(usage="%convert-p2v.py -f <FQDN>  [-i <IP>] | [-v <Num_VLAN>] | [-c <Num_Demande_Sysadmin>]", version="%prog 1.0")
   parser.add_option("-f","--fqdn", action="store", type="string", dest="vm_name",help="FDQN du serveur physique a virtualiser", metavar="FQDN" )
   parser.add_option("-i", "--ip", action="store", type="string", dest="physique_name",default="1.1.1.2",help="IP de communication entre le xen0 et le serveur physique, defaut: 1.1.1.2", metavar="IP")
   parser.add_option("-v","--vlan", action="store", type="string", dest="vlan",help="VLAN Commun entre le xen et le serveur physique", metavar="Num VLAN")
